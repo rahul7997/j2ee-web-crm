bf3ed24d28a6c97f306cfefa1e0d3565f3bade52
Rahul <32811705+rahul7997@users.noreply.github.com>
2019-09-15 18:43:24 +0530

    Add files via upload

---
+++ Variables.go
@@ -0,0 +1,21 @@
+package main
+import ("fmt")
+
+func main(){
+	var num1 int = 10
+	var num2 = 5
+	var result = num1+num2
+	fmt.Println(result)
+
+	var num3 int
+	var num4 = 2
+	num3 = 8
+	result = num3+num4
+	fmt.Println(result)
+
+	var num5 int = 30
+	fmt.Println(num5)
+
+	num6 := 40
+	fmt.Println(num6)
+}
